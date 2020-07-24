d8a94faea484a43612e295a92242ad31214e91dd
Anson Nickel <anson.nickel@gmail.com>
2020-07-24 03:08:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,7 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts "- exit : exit this program"
+  puts "- exit : exits this program"
   prompt
 end
 
