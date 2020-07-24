303beb311e5433be16e20c088b96f072553473e9
Anson Nickel <anson.nickel@gmail.com>
2020-07-24 03:33:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -48,22 +48,39 @@ def exit_jukebox
 end
 
 def run(songs)
-  prompt
-  input = gets.strip
-  life = 1
-  while life == 1
+  while true
+    prompt
+    input = gets.strip
     if input === "help"
       help
-      run(songs)
     elsif input === "list"
       list(songs)
-      run(songs)
     elsif input === "play"
       play(songs)
-      run(songs)
     elsif input === "exit"
       exit_jukebox
       break
+    end
+  end
+end
+
+# Add your code here
+def run(songs)
+  while true do
+    puts "Please enter a command:"
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
     end
   end
 end
