0309f43af5f9d6d56531859d9a8c748cf5a75290
Anson Nickel <anson.nickel@gmail.com>
2020-07-24 03:08:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,7 +16,7 @@ def prompt
   puts "Please enter a command:"
 end
 
-def help_command
+def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
